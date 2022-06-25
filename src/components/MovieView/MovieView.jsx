b155c4366f02fa65226435612ef3219160c58a91
Alexander Ratanas <alexanderratanas@gmail.com>
2022-06-25 02:53:16 -0500

    basic page view display

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -13,10 +13,18 @@ const MovieView = () => {
             payload: params.id
         })
    }, [])
-   console.log(pageData[0])
     return (
         <div>
             <h2>this is a view for {params.id}</h2>
+            {pageData.map(item => (
+                <div key={item.id}>
+                    <h2>{item.title}</h2>
+                    <img src={item.poster}/>
+                    <p>{item.description}</p>
+                </div>
+                
+            ))}
+            
             <p></p>
         </div>
         
